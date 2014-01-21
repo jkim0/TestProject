047b445f40f78340174d7fe4ea5431c235b0b708
Zuckay <yjk891@gmail.com>
2014-01-21 21:57:03 +0900

    /Service /

---
+++ TestYang/Service_y/src/com/example/service_y/MainActivity.java
@@ -0,0 +1,148 @@
+package com.example.service_y;
+
+import com.example.service_y.MainActivity;
+import com.example.service_y.R;
+import com.example.service_y.SimpleService;
+
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.res.Resources.NotFoundException;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements OnClickListener {
+	
+	TextView mTextview = null; 
+	private Button startServiceButton;
+	private Button stopServiceButton;
+	private Button checkAliveButton;
+	
+	/****/
+	private boolean isBound;
+	ISimpleAIDL mSimpleService;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		startServiceButton = (Button)findViewById(R.id.button1);		
+		stopServiceButton = (Button)findViewById(R.id.button2);
+		checkAliveButton = (Button)findViewById(R.id.button3);
+		
+		startServiceButton.setOnClickListener(this);
+		stopServiceButton.setOnClickListener(this);
+		checkAliveButton.setOnClickListener(this);
+	/*	checkAliveButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+			}
+		});*/ //정신차리길!!!
+		Intent intent = new Intent(this, SimpleService.class);
+		startService(intent); //바인드 서비스 해주고, 콜메서드 해줄거야
+		bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
+	
+	}
+
+	
+/*****************대빵 중요해!*******************/
+	
+	private ServiceConnection mServiceConnection = new ServiceConnection() {
+		@Override
+		public void onServiceConnected(ComponentName pComponentName, IBinder pIBinder) {
+			// TODO Auto-generated method stub
+			Toast.makeText(MainActivity.this, "onService",Toast.LENGTH_LONG);
+			mSimpleService = ISimpleAIDL.Stub.asInterface(pIBinder);
+			// register listener
+		}
+		@Override
+		public void onServiceDisconnected(ComponentName pComponentName) {
+			// TODO Auto-generated method stub
+			mSimpleService = null;
+		}
+	};
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+
+		Intent intent = new Intent(this, SimpleService.class);
+		int time=0;
+				switch(v.getId()){
+					case R.id.button1: //
+						//Start service : 1)바인드 서비스 2)메서드 호출 3)toast
+						//bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
+						Toast.makeText(this, "come5", Toast.LENGTH_LONG).show();
+						isBound=true;
+			//여기서 질문하기, 왜 for문으로 무한 못돌려?
+						if(isBound){
+					
+								try {
+								//	time=mSimpleService.add(0);
+									Log.i("TEST", "mtime :" + mSimpleService.add(0));
+									Toast.makeText(this, "okay",Toast.LENGTH_SHORT).show();
+									for (time=0; time <10; time++){
+									 time =mSimpleService.add(time);
+										Toast.makeText(this, time + ":main", Toast.LENGTH_SHORT).show();	
+									}
+									} catch (RemoteException e) { //catch Blcok
+									// TODO Auto-generated catch block
+								//	e.printStackTrace(); //error흔적 만들어주는 것	
+								}
+						}
+						/*thread 구현 		
+						Thread  t = new mThread();
+						t.start(); */
+						
+						//	mTextview.setText(intent.getExtras().toString());
+						break;
+					case R.id.button2:
+					//	unbindService(mServiceConnection);
+						stopService(new Intent(this, SimpleService.class));
+						break;
+					case R.id.button3:
+						sendBroadcast(new Intent("com.example.action.isAlive"));
+						//i= intent.getExtras().getInt("printing_number");
+						break;
+					}
+	}
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	/* thread구현부분 *
+	public class mThread extends Thread {
+		int mCount = 0;
+		@Override
+		public void run() {
+			while(mCount <1000) {
+				++mCount;
+				Log.i("TEST", "mCount :" + mCount);
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		*/
+		
+	}
+	
+	
+
---
+++ TestYang/Service_y/src/com/example/service_y/SimpleService.java
@@ -0,0 +1,116 @@
+package com.example.service_y;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+import android.widget.Toast;
+
+public class SimpleService extends Service {
+	int mCount = 0;
+	int i=0;
+	
+	
+	private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+		
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			// TODO Auto-generated method stub
+			Toast.makeText(context,"onRECEIVE" ,Toast.LENGTH_LONG).show();
+		}
+	}; 
+
+	@Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+	if(mReceiver!= null){
+		unregisterReceiver(mReceiver);
+	}
+	Toast.makeText(this, "onDestroy", Toast.LENGTH_LONG).show();
+	//	super.onDestroy();
+	}
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+	// TODO Auto-generated method stub
+		Toast.makeText(this, "onStartCommand", Toast.LENGTH_LONG).show();
+		IntentFilter filter = new IntentFilter("com.example.action.isAlive");
+		registerReceiver(mReceiver, filter);
+	
+		mThread sThread = new mThread();
+		sThread.start();
+		
+		/*for(int i=0; i<3; i++){
+			Toast.makeText(this, i, Toast.LENGTH_LONG).show();
+		}*/
+		
+		
+		return Service.START_NOT_STICKY;
+		//	return super.onStartCommand(intent, flags, startId);
+	}
+		
+/****서비스에서 인터페이스 구현할 차례********
+ * IsimpleAIDL.Stud()을 사용하여 AIDL에서 정의한 메서드를 구현하고, 
+ * 서비스에 바인드 될때 호출되는 onBind()메서든느 앞에서 구현한 바인더 객체를 반환하도록 구현한다* 
+ *****/
+	
+	private IBinder mBinder = new ISimpleAIDL.Stub() {
+		//RemoteException?이게 뭔지 모르겟당! 질문
+		@Override
+		
+		public int add(int value) throws RemoteException {
+			// TODO Auto-generated method stub
+			mCount = value;
+	//		Toast.makeText(SimpleService.this, value +"", Toast.LENGTH_SHORT).show();
+			Toast.makeText(SimpleService.this, mCount +":add", Toast.LENGTH_SHORT).show();
+			Log.i("TEXT","333mcount:"+mCount);
+			return mCount;
+		}
+		public String add2(long timeInMillis)throws RemoteException{
+			Date date = new Date(timeInMillis);
+			SimpleDateFormat sdf= new SimpleDateFormat("MM/dd HH:mm");
+			return sdf.format(date);
+		}
+	};
+	
+	public class mThread extends Thread {
+		boolean mCheck=true;
+		@Override
+		public void run() {
+			while(mCheck=true) {
+				++mCount;
+				Log.i("TEST","mcheck:"+mCheck);
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		Toast.makeText(this, "onBind()", Toast.LENGTH_LONG).show();
+		return mBinder;
+		//return null;
+	}
+	
+	@Override
+	public boolean onUnbind(Intent intent) {
+		// TODO Auto-generated method stub
+        Toast.makeText(this, "onUnbind()", Toast.LENGTH_LONG).show();
+		return super.onUnbind(intent);
+	}
+	
+	
+}
