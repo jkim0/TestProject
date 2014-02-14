2c1cdaf7e70f4eb65855e0305ddbc43788e4fc17
Zuckay <yjk891@gmail.com>
2014-02-14 18:24:27 +0900

    /l/

--- Emulator/gen/com/example/emulator/R.java
+++ Emulator/gen/com/example/emulator/R.java
@@ -31,19 +31,20 @@ public final class R {
         public static final int stat_sample=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f0a000e;
-        public static final int bssid=0x7f0a0009;
+        public static final int action_settings=0x7f0a000f;
+        public static final int bluetooth_switch=0x7f0a0005;
+        public static final int bssid=0x7f0a000a;
         public static final int btn_start=0x7f0a0002;
         public static final int btn_stop=0x7f0a0001;
         public static final int btn_wifi=0x7f0a0003;
-        public static final int capabilities=0x7f0a000b;
-        public static final int frequency=0x7f0a000c;
-        public static final int level=0x7f0a000d;
-        public static final int spinner1=0x7f0a0006;
-        public static final int spinner2=0x7f0a0007;
-        public static final int spinner_tv1=0x7f0a0005;
-        public static final int spinner_tv2=0x7f0a0008;
-        public static final int ssid=0x7f0a000a;
+        public static final int capabilities=0x7f0a000c;
+        public static final int frequency=0x7f0a000d;
+        public static final int level=0x7f0a000e;
+        public static final int spinner1=0x7f0a0007;
+        public static final int spinner2=0x7f0a0008;
+        public static final int spinner_tv1=0x7f0a0006;
+        public static final int spinner_tv2=0x7f0a0009;
+        public static final int ssid=0x7f0a000b;
         public static final int textView1=0x7f0a0000;
         public static final int wifi_switch=0x7f0a0004;
     }
@@ -61,18 +62,22 @@ public final class R {
         public static final int upload_ok=0x7f040002;
     }
     public static final class string {
-        public static final int action_settings=0x7f070001;
-        public static final int app_name=0x7f070000;
-        public static final int bluetooth=0x7f07000a;
-        public static final int broadcast=0x7f07000b;
-        public static final int choose_one_section=0x7f070008;
-        public static final int hello_world=0x7f070002;
-        public static final int remote_service_label=0x7f070006;
-        public static final int remote_service_started=0x7f070005;
-        public static final int spinner=0x7f070007;
-        public static final int start=0x7f070003;
-        public static final int stop=0x7f070004;
-        public static final int wifi=0x7f070009;
+        public static final int action_settings=0x7f070005;
+        public static final int app_name=0x7f070004;
+        public static final int bluetooth=0x7f07000e;
+        public static final int bluetooth_text=0x7f070003;
+        public static final int broadcast=0x7f07000f;
+        public static final int choose_one_section=0x7f07000c;
+        public static final int hello_world=0x7f070006;
+        public static final int off=0x7f070001;
+        public static final int on=0x7f070000;
+        public static final int remote_service_label=0x7f07000a;
+        public static final int remote_service_started=0x7f070009;
+        public static final int spinner=0x7f07000b;
+        public static final int start=0x7f070007;
+        public static final int stop=0x7f070008;
+        public static final int wifi=0x7f07000d;
+        public static final int wifi_text=0x7f070002;
     }
     public static final class style {
         /** 
