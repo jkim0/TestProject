ef0db6792a29d26a6e83b58fbe65e11c453d6d8c
yyoun <wsadghjk630@naver.com>
2014-02-27 16:44:22 +0900

    Final Emulator

---
+++ final_Emulator/src/com/example/emulator/Information.java
@@ -0,0 +1,16 @@
+package com.example.emulator;
+
+
+public class Information{
+	public String wifi;
+	public String bluetooth;
+	public String screen;
+	
+	public Information()
+	{
+		wifi = "OFF";
+		bluetooth = "OFF";
+		screen = "OFF";
+	}
+}
+
